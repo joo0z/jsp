150996dd7c468234b68d3e96409bd17b0aeafc72
joo <wnjd63232@gmail.com>
2020-11-06 13:51:02 +0900

    "spring error page 설정  . mvc:resources"

--- src/main/java/kr/or/ddit/mvc/exception/web/ExceptionAdvice.java
+++ src/main/java/kr/or/ddit/mvc/exception/web/ExceptionAdvice.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 public class ExceptionAdvice {
 	private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
--- src/main/java/kr/or/ddit/mvc/exception/web/ExceptionController.java
+++ src/main/java/kr/or/ddit/mvc/exception/web/ExceptionController.java
@@ -43,4 +43,5 @@ public class ExceptionController {
 //		return "";
 	}
 	
+	
 }
